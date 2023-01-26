2b491dd6682002677dd5698e8cda87395e7d9961
Konstantin Chukhlomin <mail@chuhlomin.com>
2023-01-26 00:04:15 -0500

    refactor: list of file changes between tags

--- generator.go
+++ generator.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/format/diff"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
@@ -44,14 +43,7 @@ func (g *generator) Run() error {
 		return fmt.Errorf("render index: %w", err)
 	}
 
-	log.Printf("Getting diffs")
-	forward, backward, err := getDiff(g.repo, tags)
-	if err != nil {
-		return fmt.Errorf("get renames: %w", err)
-	}
-
-	log.Printf("Rendering files changes")
-	if err := g.renderFilesChanges(tags, forward, backward); err != nil {
+	if err := g.renderFilesChanges(tags); err != nil {
 		return fmt.Errorf("render files: %w", err)
 	}
 
@@ -96,24 +88,24 @@ func (g *generator) renderIndex(tags []tag) error {
 }
 
 type file struct {
-	Name       string
-	ChangeType string // A, D, M, R
-	OldName    string
+	Name      string
+	OldName   string
+	Operation string // A, D, M, R
 }
 
 func (f file) Less(other file) bool {
 	return f.Name < other.Name
 }
 
-func (g *generator) renderFilesChanges(tags []tag, forward, backward []patch) error {
+func (g *generator) renderFilesChanges(tags []tag) error {
 	// get all combinations of tags
 	for _, tag1 := range tags {
 		for _, tag2 := range tags {
-			patches := forward
-			if tag1.Version() > tag2.Version() {
-				patches = backward
+			log.Printf("Rendering files changes between %s and %s", tag1.Name, tag2.Name)
+			if err := g.renderFilesChangesBetweenTags(tag1, tag2); err != nil {
+				return fmt.Errorf("render files for tags %s -> %s: %w", tag1.Name, tag2.Name, err)
 			}
-			if err := g.renderFilesChangesForTags(tag1, tag2, patches); err != nil {
+			if err := g.renderFilesChangesBetweenTags(tag2, tag1); err != nil {
 				return fmt.Errorf("render files for tags %s -> %s: %w", tag1.Name, tag2.Name, err)
 			}
 		}
@@ -121,7 +113,12 @@ func (g *generator) renderFilesChanges(tags []tag, forward, backward []patch) er
 	return nil
 }
 
-func (g *generator) renderFilesChangesForTags(tag1, tag2 tag, patches []patch) error {
+func (g *generator) renderFilesChangesBetweenTags(tag1, tag2 tag) error {
+	changes, err := g.diff(tag1, tag2)
+	if err != nil {
+		return fmt.Errorf("collect changes: %w", err)
+	}
+
 	if err := os.MkdirAll(filepath.Join("output", "files", tag1.Name), 0755); err != nil {
 		return fmt.Errorf("create output/files/%s: %w", tag1.Name, err)
 	}
@@ -131,101 +128,6 @@ func (g *generator) renderFilesChangesForTags(tag1, tag2 tag, patches []patch) e
 		return fmt.Errorf("create output/files/%s/%s.html: %w", tag1.Name, tag2.Name, err)
 	}
 
-	if tag1 == tag2 {
-		if err := g.tmpl.ExecuteTemplate(f, "files.gohtml", struct {
-			Tag1    string
-			Tag2    string
-			Changes []file
-		}{
-			Tag1:    tag1.Name,
-			Tag2:    tag2.Name,
-			Changes: nil,
-		}); err != nil {
-			return fmt.Errorf("execute template: %w", err)
-		}
-		return nil
-	}
-
-	// log.Printf("Collecting changes between %s and %s", tag1.Name, tag2.Name)
-
-	// collect all changes between the two tags
-	changes := map[string]file{}
-
-	collecting := false
-	for _, patch := range patches {
-		if collecting {
-			// log.Printf("%s -> %s (%d)", patch.from, patch.to, len(patch.changes))
-
-			for _, fileChange := range patch.changes {
-				if fileChange.IsBinary() {
-					continue
-				}
-
-				from, to := fileChange.Files()
-
-				if from == nil {
-					changes[to.Path()] = file{
-						Name:       to.Path(),
-						ChangeType: "A",
-					}
-					continue
-				}
-
-				if to == nil {
-					changes[from.Path()] = file{
-						Name:       from.Path(),
-						ChangeType: "D",
-					}
-					continue
-				}
-
-				if from.Path() != to.Path() {
-					changes[to.Path()] = file{
-						Name:       to.Path(),
-						ChangeType: "R",
-						OldName:    from.Path(),
-					}
-					if _, ok := changes[from.Path()]; ok {
-						delete(changes, from.Path())
-					}
-					continue
-				}
-
-				if from.Path() == to.Path() {
-					changes[to.Path()] = file{
-						Name:       to.Path(),
-						ChangeType: "M",
-					}
-					continue
-				}
-			}
-		}
-
-		if patch.to == tag1.Name || patch.to == tag2.Name {
-			// log.Printf("Collecting toggle by tag %s", patch.to)
-			collecting = !collecting
-			if !collecting {
-				break
-			}
-		}
-	}
-
-	if collecting {
-		// did not find the second tag, return error
-		return fmt.Errorf("did not find second tag (%q or %q", tag1, tag2)
-	}
-
-	changesList := make([]file, 0, len(changes))
-	// log.Printf("Changes: %d", len(changes))
-	for _, change := range changes {
-		// log.Printf("  %s %s", change.ChangeType, change.Name)
-		changesList = append(changesList, change)
-	}
-
-	sort.Slice(changesList, func(i, j int) bool {
-		return changesList[i].Name < changesList[j].Name
-	})
-
 	if err := g.tmpl.ExecuteTemplate(f, "files.gohtml", struct {
 		Tag1    string
 		Tag2    string
@@ -233,7 +135,7 @@ func (g *generator) renderFilesChangesForTags(tag1, tag2 tag, patches []patch) e
 	}{
 		Tag1:    tag1.Name,
 		Tag2:    tag2.Name,
-		Changes: changesList,
+		Changes: changes,
 	}); err != nil {
 		return fmt.Errorf("execute template: %w", err)
 	}
@@ -366,6 +268,63 @@ func (g *generator) copyEmbeddedStaticFiles() error {
 	return nil
 }
 
+func (g *generator) diff(tag1, tag2 tag) ([]file, error) {
+	commit1, err := g.repo.CommitObject(tag1.Hash)
+	if err != nil {
+		return nil, fmt.Errorf("get commit for tag %q: %w", tag1.Name, err)
+	}
+
+	commit2, err := g.repo.CommitObject(tag2.Hash)
+	if err != nil {
+		return nil, fmt.Errorf("get commit for tag %q: %w", tag2.Name, err)
+	}
+
+	p, err := commit1.Patch(commit2)
+	if err != nil {
+		return nil, fmt.Errorf("get patch: %w", err)
+	}
+
+	patches := p.FilePatches()
+	changes := make([]file, 0, len(patches))
+	for _, patch := range patches {
+		if patch.IsBinary() {
+			continue
+		}
+
+		from, to := patch.Files()
+
+		var toPath, fromPath string
+		if to != nil {
+			toPath = to.Path()
+		}
+		if from != nil {
+			fromPath = from.Path()
+		}
+
+		changes = append(changes, file{
+			Name:    toPath,
+			OldName: fromPath,
+			Operation: func(to, from string) string {
+				if from == "" {
+					return "A"
+				}
+
+				if to == "" {
+					return "D"
+				}
+
+				if from != to {
+					return "R"
+				}
+
+				return "M"
+			}(toPath, fromPath),
+		})
+	}
+
+	return changes, nil
+}
+
 func copyFile(src, dst string) error {
 	in, err := os.Open(src)
 	if err != nil {
@@ -449,70 +408,5 @@ type patch struct {
 	to       string
 	fromHash string
 	toHash   string
-	changes  []diff.FilePatch
-}
-
-func getDiff(r *git.Repository, tags []tag) ([]patch, []patch, error) {
-	sort.Slice(tags, func(i, j int) bool {
-		// sort acsending
-		return tags[i].Version() < tags[j].Version()
-	})
-
-	forward, err := getPatches(r, tags)
-	if err != nil {
-		return nil, nil, fmt.Errorf("get forward patches: %w", err)
-	}
-
-	sort.Slice(tags, func(i, j int) bool {
-		// sort descending
-		return tags[i].Version() > tags[j].Version()
-	})
-
-	backward, err := getPatches(r, tags)
-	if err != nil {
-		return nil, nil, fmt.Errorf("get forward patches: %w", err)
-	}
-
-	return forward, backward, nil
-}
-
-func getPatches(r *git.Repository, tags []tag) ([]patch, error) {
-	var result []patch
-
-	var commitPrev *object.Commit
-	var tagPrev string
-	for _, tag := range tags {
-		commit, err := r.CommitObject(tag.Hash)
-		if err != nil {
-			return nil, fmt.Errorf("get commit for tag %q: %w", tag.Name, err)
-		}
-
-		if commitPrev == nil {
-			result = append(result, patch{
-				from:   tagPrev,
-				to:     tag.Name,
-				toHash: commit.Hash.String(),
-			})
-			commitPrev = commit
-			tagPrev = tag.Name
-			continue
-		}
-
-		p, err := commitPrev.Patch(commit)
-		if err != nil {
-			return nil, fmt.Errorf("get patch: %w", err)
-		}
-
-		result = append(result, patch{
-			from:     tagPrev,
-			to:       tag.Name,
-			fromHash: commitPrev.Hash.String(),
-			toHash:   commit.Hash.String(),
-			changes:  p.FilePatches(),
-		})
-		tagPrev = tag.Name
-		commitPrev = commit
-	}
-
-	return result, nil
+	changes  []file
 }
